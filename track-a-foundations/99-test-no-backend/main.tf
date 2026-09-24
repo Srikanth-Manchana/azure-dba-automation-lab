b8ba85cc8994e2094512adb287fe50f9a1960453
Srikanth Manchana <smanchana.work@gmail.com>
2026-09-24 15:25:22 -0700

    test: add module without backend.tf to prove enforcement fails

---
+++ track-a-foundations/99-test-no-backend/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-test-no-backend"
+  location = "westus2"
+}
